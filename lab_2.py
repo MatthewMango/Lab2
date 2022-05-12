cabf65f9a22f4c98f33380dffd04653dca01d9a0
MatthewMango <98561675+MatthewMango@users.noreply.github.com>
2022-05-11 19:23:23 -0700

    fixed grammer

--- lab_2.py
+++ lab_2.py
@@ -6,21 +6,4 @@ round answer to next number
 output the results to the user
 end
 """
-import math
 
-# input: sheet
-def calculate(sheet):
-    # step 1
-    answer = sheet / 5
-    # step 2
-    rounded = round(answer)
-    print("sheet is : ", sheet)
-    print("The answer is: ", answer)
-    print("rounded is: ", answer)
-    print("=================================")
-    # output: number of stamps needed
-    return rounded
-
-output = calculate(10000)
-
-print("the return statement is: ", output)
